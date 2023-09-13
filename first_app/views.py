fed9e1af0ffec87ef20ca28b3240f5a2c0ac1174
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-13 10:45:43 +0330

    Add basic views and urls
    - add views
    - add urls

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def employeeView(request):
+    emp = {
+        'id': 123,
+        'name': 'John',
+        'sal': 1000_000,
+    }
+
+    return JsonResponse(emp)    # Serializes the dictionary to json
