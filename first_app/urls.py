114a0795d0def07ebcb460a4dc686b268a72b56f
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-13 13:09:52 +0330

    Add employee detail view
    - add employee detail view to retrieve, update, delete a single employee
    - add urls

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,4 +3,7 @@ from .import views
 
 urlpatterns = [
     path('emps/', views.employeeView),
+    path('employees/', views.employee_list),
+    path('employees/<int:pk>', views.employee_detail),
+
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -31,4 +31,27 @@ def employee_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def employee_detail(request, pk):
+    try:
+        employee = Employee.objects.get(pk=pk)
+    except Employee.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        employee.delete()
+        return Response(status= status.HTTP_204_NO_CONTENT)
+
+
 
