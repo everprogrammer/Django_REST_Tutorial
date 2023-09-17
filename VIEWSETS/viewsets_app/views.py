d2dac41b0c54e84e7081e3951515452fc383ff2e
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-17 11:43:11 +0330

    Add filtering
    - add filtering in viewsets/views.py
    - install django-filter
    - update requirements.txt

--- VIEWSETS/viewsets_app/views.py
+++ VIEWSETS/viewsets_app/views.py
@@ -2,16 +2,29 @@ from rest_framework import viewsets
 from .models import Employee
 from .serializers import EmployeeSerializer
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 # create your views here
 # Supports primary and non-primary key based restful endpoints
 class EmployeePagination(PageNumberPagination):
-    page_size = 1
+    page_size = 3
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     pagination_class = EmployeePagination # this class overrides the default in settings
+    # filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['name', 'sal']
 
+    ## Search filter
+    # filter_backends = [filters.SearchFilter]
+    # search_fields = ['^id', '^name'] ## starts with search word
+                                    ## '=name' > search word is exactly the name
+
+    ## OrderingFilter
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['name', 'sal']
+
+    
 
