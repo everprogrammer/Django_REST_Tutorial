7162df5506739922e1ba7a3a22fd5bacfde82930
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-17 10:53:51 +0330

    Add pagination
    - add pagination settings to viewsets/settings.py
    - add custom pagination in viewsets/views.py

--- VIEWSETS/viewsets_app/views.py
+++ VIEWSETS/viewsets_app/views.py
@@ -1,10 +1,17 @@
 from rest_framework import viewsets
 from .models import Employee
 from .serializers import EmployeeSerializer
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
 # create your views here
 # Supports primary and non-primary key based restful endpoints
+class EmployeePagination(PageNumberPagination):
+    page_size = 1
+
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    pagination_class = EmployeePagination # this class overrides the default in settings
+
+
