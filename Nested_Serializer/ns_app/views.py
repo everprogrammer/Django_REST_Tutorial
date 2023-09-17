54f0f6ab75492808471e3445e6c5cb30eb8c6538
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-17 12:17:49 +0330

    - Add basic authentication/authorization
    - Change default settings for authentication/authorization in Nested_Serializer/settings.py

--- Nested_Serializer/ns_app/views.py
+++ Nested_Serializer/ns_app/views.py
@@ -1,11 +1,17 @@
 from rest_framework import generics
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 # Create your views here.
 class AuthorListView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+    # Instead of copying and pasting the below to all classes, use it as global
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
